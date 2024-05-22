5a1cc30c16892f68d9a99085276def6522582f6d
Marcy <madisontolentino@gmail.com>
2024-05-22 17:35:07 -0400

    incomplete

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+  event.preventDefault()
+
 };
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
+
+const spanValues = (event) => {
+  event.preventDefault()
+
+  const userInput = document.querySelector('#username')
+  const codingLevelInput = document.querySelector('codingLevel')
+  const locationInput = document.querySelector('#location')
+  const assignmentInput = document.querySelector('#did-like-assignment')
+
+  const nameSpan = document.querySelector('#results-username')
+  const codingLevelSpan = document.querySelector('#results-coding-level')
+  const locationSpan = document.querySelector('#results-location')
+  const assignmentSpan = document.querySelector('#results-did-like-assignment')
+  
+
+  nameSpan.textContent = userInput.value.toLowerCase()
+  codingLevelSpan.textContent = codingLevelInput.value.toLowerCase()
+  locationSpan.textContent = locationInput.value.toLowerCase()
+  assignmentSpan.textContent = assignmentInput.checked ? 'Yes': 'No'
+}
+
+document.querySelector('form').addEventListener('submit', spanValues)
