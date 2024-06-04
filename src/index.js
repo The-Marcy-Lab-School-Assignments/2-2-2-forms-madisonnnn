f26c878ccfb73c82721c59d18deb1e20d498ac6e
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-04 14:57:49 -0400

    feedback

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /** GENERAL FEEDBACK:
- *
+ * You are so close! I left feedback on where you can improve! You got this!
  */
 
+/** FEEDBACK: If you do not need this handler and event listener, make sure to remove it to keep your code clean! */
 const handleSubmit = (event) => {
   event.preventDefault()
 
@@ -13,6 +14,7 @@ const spanValues = (event) => {
   event.preventDefault()
 
   const userInput = document.querySelector('#username')
+  /** FEEDBACK: Are you getting the correct input here? Hint: event.target. How can you target the coding level input by using the name attribute? */
   const codingLevelInput = document.querySelector('codingLevel')
   const locationInput = document.querySelector('#location')
   const assignmentInput = document.querySelector('#did-like-assignment')
@@ -22,11 +24,12 @@ const spanValues = (event) => {
   const locationSpan = document.querySelector('#results-location')
   const assignmentSpan = document.querySelector('#results-did-like-assignment')
   
-
+  /** FEEDBACK: You do not need to lowercase the values since we are getting the user inputs! */
   nameSpan.textContent = userInput.value.toLowerCase()
   codingLevelSpan.textContent = codingLevelInput.value.toLowerCase()
   locationSpan.textContent = locationInput.value.toLowerCase()
   assignmentSpan.textContent = assignmentInput.checked ? 'Yes': 'No'
+  /** FEEDBACK: Do not forget to reset the form when you are done collecting the data and displaying it! */
 }
 
 document.querySelector('form').addEventListener('submit', spanValues)
