7c6810181b51fd2932b57f26db062f39e9711c3d
Marcy <madisontolentino@gmail.com>
2024-06-12 11:43:49 -0400

    updating

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
  *
  */
 
+//itzel this made me very sad :(
+
 const handleSubmit = (event) => {
   event.preventDefault()
 
@@ -12,21 +14,35 @@ document.querySelector('form').addEventListener('submit', handleSubmit)
 const spanValues = (event) => {
   event.preventDefault()
 
+  const form = event.target
+  //grabbing input data
   const userInput = document.querySelector('#username')
-  const codingLevelInput = document.querySelector('codingLevel')
+  const codingLevelInput = document.querySelector('fieldset#codingLevel')
   const locationInput = document.querySelector('#location')
   const assignmentInput = document.querySelector('#did-like-assignment')
 
-  const nameSpan = document.querySelector('#results-username')
-  const codingLevelSpan = document.querySelector('#results-coding-level')
-  const locationSpan = document.querySelector('#results-location')
-  const assignmentSpan = document.querySelector('#results-did-like-assignment')
-  
+  //grabbing span
+  const nameSpan = document.querySelector('span#results-username')
+  const codingLevelSpan = document.querySelector('span#results-coding-level')
+  const locationSpan = document.querySelector('span#results-location')
+  const assignmentSpan = document.querySelector('span#results-did-like-assignment')
 
-  nameSpan.textContent = userInput.value.toLowerCase()
-  codingLevelSpan.textContent = codingLevelInput.value.toLowerCase()
-  locationSpan.textContent = locationInput.value.toLowerCase()
+  //location things wow
+  const brooklyn = locationInput.querySelector('#brooklyn')
+  const otherBorough = locationInput.querySelector('#other-borough')
+  const outOfState = locationInput.querySelector('#out-of-state')
+
+  if(brooklyn.selected === true) locationSpan.textContent = brooklyn.value
+  if(otherBorough.selected === true) locationSpan.textContent = otherBorough.value
+  if(outOfState.selected === true) locationSpan.textContent = outOfState.value
+  
+  //changing span data to input value
+  nameSpan.textContent = userInput.value
+  codingLevelSpan.textContent = codingLevelInput.value
   assignmentSpan.textContent = assignmentInput.checked ? 'Yes': 'No'
+
+  form.reset()
 }
+
 
 document.querySelector('form').addEventListener('submit', spanValues)
